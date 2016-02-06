5f564cbe01244a2f9cae3d757e1b1473c053c7c7
Hugo <ringhugos@gmail.com>
2016-02-05 20:57:11 -0600

    Filas para cada quien

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
-<div>
-  Vamos haciendo el primer cambio.
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+        <title>Aprender GitHub</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="images/hugui.ico" type="image/icon">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+	</head>
+
+	<body>
+
+	<section class="container-fluid">
+      <div class="container">
+			     <div style="margin: 0;" class="row">
+				         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    Robinson
+                 </div>
+		       </div>
+     </div>
+	</section>
+
+  <section class="container-fluid">
+      <div class="container">
+			     <div style="margin: 0;" class="row">
+				         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    Oscar
+                 </div>
+		       </div>
+     </div>
+	</section>
+
+  <section class="container-fluid">
+      <div class="container">
+			     <div style="margin: 0;" class="row">
+				         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    Hugo
+                 </div>
+		       </div>
+     </div>
+	</section>
+
+	</body>
+</html>
