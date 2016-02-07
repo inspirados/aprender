448543ee30463ac07229dc26b3b455fd9ae2307d
Hugo <ringhugos@gmail.com>
2016-02-07 10:16:50 -0600

    Agregando coluna de testimonio

--- index.php
+++ index.php
@@ -18,12 +18,12 @@
                                              <img src="imagenes/linux.png" alt=""/>
                     <h2>Contenido de Robinson</h2>
                     <p>Hola Hugo, <br>Gracias por dejarme participar en esta iniciativa</p>
-                    
+
                     <?php include 'robinson.php';?>
-                    
-                    
-                    
-                    
+
+
+
+
                  </div>
 		       </div>
      </div>
@@ -42,8 +42,18 @@
   <section class="container-fluid">
       <div class="container">
 			     <div style="margin: 0;" class="row">
-				         <div id="hugo" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    Acá voy empezando mi parte en la que agregaré algunos formularios o lo que sea
+				         <div class="hugo" class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+										 <div class="testimonio row">
+												 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+													 <img class="img-circle img-responsive" src="imagenes/fotoTestimonio.jpg">
+												 </div>
+												 <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 jumbotron jumbotronTestimonio">
+													 Juanita la bella
+												 </div>
+												 <div class="col-lg-12 testi">
+													 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu dapibus odio. In vulputate facilisis libero sed volutpat.
+												 </div>
+						 				 </div>
                  </div>
 		       </div>
      </div>
